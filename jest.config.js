d61e154fd0bc096f04a3effd5a75169ec0ab3b21
beeftongue551 <login@beeftongue551.org>
2022-11-21 23:52:04 +0900

    Commit config files

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: ['module:metro-react-native-babel-preset'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    env: {
+        production: {
+            plugins: ['react-native-paper/babel'],
+        },
+    },
+};
